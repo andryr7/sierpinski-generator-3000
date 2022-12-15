ff6b97051088a3e01ae98efaffe58fde613906e4
unknown <andryratsimba@gmail.com>
2022-12-15 16:44:45 +0100

    working version with double state use

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,92 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import styled from "styled-components";
+import { useEffect, useState } from 'react';
+import { useStateWithCallback } from "./useStateWithCallback";
+
+const StyledApp = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: navy;
+`
+
+const StyledSTContainer = styled.div`
+  width: 550px;
+  height: 550px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  flex-direction: column;
+`
+
+const SerpinskiTriangle = () => {
+  const [firstPoint, setFirstPoint] = useState({id: 1, x: 250, y: 0});
+  const [secondPoint, setDecondPoint] = useState({id: 2, x: 500, y: 500});
+  const [thirdPoint, setThirdPoint] = useState({id: 3, x: 0, y: 500});
+  const [startPoint, setStartPoint] = useState({id: 4, x: 300, y:300});
+  const [lastPoint, setLastPoint] = useState(startPoint);
+  const [pointList, setPointList] = useState([
+    firstPoint, secondPoint, thirdPoint, startPoint
+  ]);
+
+  const getRandomPoint = () => {
+    switch (Math.floor(Math.random() * 3)) {
+      case 0:
+        return (firstPoint);
+      case 1:
+        return (secondPoint);
+      case 2:
+        return (thirdPoint);
+    };
+  };
+
+  const handleNewPointClick = () => {
+    drawNewPoint();
+  };
+
+  const drawNewPoint = () => {
+    const referencepoint = getRandomPoint();
+    const newPoint = {
+      id: lastPoint.id + 1,
+      x: Math.round((lastPoint.x + referencepoint.x)/2*100)/100,
+      y: Math.round((lastPoint.y + referencepoint.y)/2*100)/100,
+    };
+    setLastPoint(newPoint);
+    setPointList((current)=>[...current, newPoint]);
+  };
+
+  useEffect(()=>{
+    console.log('render');
+    console.log(lastPoint);
+  })
+
+  return(
+    <>
+      <button onClick={()=>{handleNewPointClick()}}>Draw new point</button>
+      <button >Stop drawing points</button>
+      <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      height="500"
+      width="500"
+      >
+        {pointList.map((point)=>(
+          <circle key={point.id} cx={point.x} cy={point.y} r="1" stroke="black" fill="black" strokeWidth="2"/>
+        ))}
+      </svg>
+    </>
+  ) 
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <StyledApp>
+      <StyledSTContainer>
+        <SerpinskiTriangle/>
+      </StyledSTContainer>
+    </StyledApp>
   )
 }
 
