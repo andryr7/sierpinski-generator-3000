5c941db50cb806bf538c62e20d5cbb5e124292bd
unknown <andryratsimba@gmail.com>
2022-12-15 17:04:10 +0100

    reduced new point method to one setState

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,6 @@ const SerpinskiTriangle = () => {
   const [secondPoint, setDecondPoint] = useState({id: 2, x: 500, y: 500});
   const [thirdPoint, setThirdPoint] = useState({id: 3, x: 0, y: 500});
   const [startPoint, setStartPoint] = useState({id: 4, x: 300, y:300});
-  const [lastPoint, setLastPoint] = useState(startPoint);
   const [pointList, setPointList] = useState([
     firstPoint, secondPoint, thirdPoint, startPoint
   ]);
@@ -48,24 +47,20 @@ const SerpinskiTriangle = () => {
 
   const drawNewPoint = () => {
     const referencepoint = getRandomPoint();
+    const lastPoint = pointList[pointList.length-1];
     const newPoint = {
       id: lastPoint.id + 1,
       x: Math.round((lastPoint.x + referencepoint.x)/2*100)/100,
       y: Math.round((lastPoint.y + referencepoint.y)/2*100)/100,
     };
-    setLastPoint(newPoint);
     setPointList((current)=>[...current, newPoint]);
   };
-
-  useEffect(()=>{
-    console.log('render');
-    console.log(lastPoint);
-  })
 
   return(
     <>
       <button onClick={()=>{handleNewPointClick()}}>Draw new point</button>
       <button >Stop drawing points</button>
+      <span>{pointList.length}</span>
       <svg 
       xmlns="http://www.w3.org/2000/svg" 
       height="500"
